d553a2b14d294b39df79de4fa36debe02c971b3d
Howzera <raul-almeida95@hotmail.com>
2020-02-19 21:20:50 +0000

    CURSO EM VIDEO PYTHON

--- #066 - Vários números com flag.py
+++ #066 - Vários números com flag.py
@@ -3,3 +3,19 @@
 #No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 #https://youtu.be/d2ug6quC1bk
 
+n = soma = cont = 0
+
+while True:
+
+    soma += n
+    
+
+    n = int(input('Digite um número: '))
+    
+    if n == 999:
+        break
+
+    cont += 1
+
+print(f'Você digitou {cont} numero e a soma entre eles foi {soma}')
+
